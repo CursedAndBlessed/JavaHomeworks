f45101698e5053aa11b3a1ca176e271d34250b28
CursedAndBlessed <8udzumyki@gmail.com>
2022-02-25 17:42:42 +0300

    Fixed

--- homeworks/task001/Main.java
+++ homeworks/task001/Main.java
@@ -7,17 +7,14 @@ public class Main {
         int b = calculator.minus.apply(1, 1);
         boolean z = calculator.isPositive.test(-20);
 
-
         calculator.println.accept(a);
         calculator.println.accept(b);
         calculator.printlnForBoolean.accept(z);
 
-        try {
-            int c = calculator.devide.apply(a, b);
-            calculator.println.accept(c);
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException("Деление на 0!");
-        }
+        int c = calculator.devide.apply(a, b);
+        calculator.println.accept(c);
+
+
 
     }
 
